7536192e08fcd9a6c50a455cf79875f69410d7e3
Dominic Pitts <dom.r.pitts@gmail.com>
2022-01-08 19:21:21 -0500

    Updated gitignore and added the ability to remove an assignment.

--- server/controllers/assignment-ctrl.ts
+++ server/controllers/assignment-ctrl.ts
@@ -1,40 +1,59 @@
-import Assignment from '../models/assignment-model';
+import Assignment from "../models/assignment-model";
 
 export const createAssignment = async (req: any, res: any) => {
-    const body = req.body;
+  const body = req.body;
 
-    if (!body) {
-        return res.status(400).json({
-            success: false,
-            error: 'Assignment not provided.',
-        });
-    }
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "Assignment not provided.",
+    });
+  }
 
-    const assignment = new Assignment(body);
+  const assignment = new Assignment(body);
 
-    if (!assignment) {
-        return res.status(400).json({
-            success: false,
-            error: 'Malformed assignment data.',
-        });
-    }
+  if (!assignment) {
+    return res.status(400).json({
+      success: false,
+      error: "Malformed assignment data.",
+    });
+  }
 
-    try {
-        await assignment.save();
+  try {
+    await assignment.save();
 
-        return res.status(201).json({
-            success: true,
-            id: assignment._id,
-            message: 'Assignment created!',
-        })
-    } catch (error: any) {
-        return res.status(400).json({
-            error,
-            message: 'Assignment creation failed.',
-        });
-    }
-}
+    return res.status(201).json({
+      success: true,
+      id: assignment._id,
+      message: "Assignment created!",
+    });
+  } catch (error: any) {
+    return res.status(400).json({
+      error,
+      message: "Assignment creation failed.",
+    });
+  }
+};
 
-module.exports = {
-    createAssignment,
+export const deleteAssignment = (req: any, res: any) => {
+  try {
+    Assignment.findOneAndDelete(
+      { _id: req.params.id },
+      (err: any, assignment: typeof Assignment) => {
+        if (err) {
+          return res.status(400).json({ success: false, error: err });
+        }
+
+        if (!assignment) {
+          return res
+            .status(404)
+            .json({ success: false, error: "Assignment not found." });
+        }
+
+        return res.status(200).json({ success: true, data: assignment });
+      }
+    );
+  } catch (err: any) {
+    console.log(err);
+  }
 };
--- server/routes/homework-router.ts
+++ server/routes/homework-router.ts
@@ -1,13 +1,14 @@
-import * as assignmentCtrl from '../controllers/assignment-ctrl';
-import * as classCtrl from '../controllers/class-ctrl';
-import { Router } from 'express';
+import * as assignmentCtrl from "../controllers/assignment-ctrl";
+import * as classCtrl from "../controllers/class-ctrl";
+import { Router } from "express";
 
 const router = Router();
 
 // Assignment
-router.post('/assignment', assignmentCtrl.createAssignment);
+router.post("/assignment", assignmentCtrl.createAssignment);
+router.delete("/assignment/:id", assignmentCtrl.deleteAssignment);
 
 // Class
-router.post('/class', classCtrl.createClass);
+router.post("/class", classCtrl.createClass);
 
 export default router;
