19a24ff5046eb6e0e07f32141325e84c5a91c182
Dominic Pitts <dom.r.pitts@gmail.com>
2022-01-16 22:33:53 -0500

    Added the ability to retrieve classes and assignments and display their names.

--- client/src/pages/AssignmentsList.tsx
+++ client/src/pages/AssignmentsList.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import * as api from '../api';
+
+const Wrapper = styled.div`
+  padding: 0 40px 40px 40px;
+`;
 
 const AssignmentsList = () => {
+  const [assignments, setClasses] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadClasses = async () => {
+    setIsLoading(true);
+
+    const assignments: any = await (await api.getAssignments()).data.data;
+
+    setClasses(assignments);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadClasses();
+  }, []);
+
   return (
     <>
-      <div>
-        <p>Assignments LIST</p>
-      </div>
+      <Wrapper>
+        {isLoading ? (
+          <div>loading assignments...</div>
+        ) : (
+          assignments.map((assignment: any, key: any) => (
+            <div>
+              <span>Class name: {assignment.name}</span>
+            </div>
+          ))
+        )}
+      </Wrapper>
     </>
   );
 };
--- client/src/pages/ClassesList.tsx
+++ client/src/pages/ClassesList.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import * as api from '../api';
+
+const Wrapper = styled.div`
+  padding: 0 40px 40px 40px;
+`;
 
 const ClassesList = () => {
+  const [classes, setClasses] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadClasses = async () => {
+    setIsLoading(true);
+
+    const classes: any = await (await api.getClasses()).data.data;
+
+    setClasses(classes);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadClasses();
+  }, []);
+
   return (
     <>
-      <div>
-        <p>CLASSES LIST</p>
-      </div>
+      <Wrapper>
+        {isLoading ? (
+          <div>loading classes...</div>
+        ) : (
+          classes.map((classObj: any, key: any) => (
+            <div>
+              <span>Class name: {classObj.name}</span>
+            </div>
+          ))
+        )}
+      </Wrapper>
     </>
   );
 };
